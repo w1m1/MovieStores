7c6e732e8477d657d81d2394295b268cc4686fa1
wm <1191273496@qq.com>
2018-08-15 08:33:24 +0800

    配置反向代理，moviedetail

--- src/Components/MovieDetail/index.js
+++ src/Components/MovieDetail/index.js
@@ -1,21 +1,63 @@
-import React,{Component} from 'react'
+import React,{Component} from 'react';
+import "./index.css";
+import {NavLink} from "react-router-dom";
+import {connect} from "react-redux";
+import axios from "axios";
 
 class MovieStore extends Component{
 	constructor(){
 		super();
 		this.state = {
-
+			dataList:null
 		}
 	}
 	render(){
 		return (
 			<div id="MovieStore">
-				MovieStore
+				<div className="head">
+					<div className="intro">
 
+					
+						<div className="img">
+								<img src={this.state.dataList}/>
+						</div>
+						
+						<div className="right"></div>
+					</div>
+				</div>
+					<p className="word">黄渤在绝境荒岛的逆袭之路</p>
+					<NavLink to="/index">查影讯/购票</NavLink>
 			</div>
 
 			)
 	}
+	componentWillMount(){
+		this.props.changeNavbar()
+
+	}
+
+	componentDidMount(){
+		axios.get('/Service/callback.mi/movie/Detail.api?movieId=225827&locationId=2908').then(res=>{
+			console.log(res.data)
+			this.setState(
+					{
+						dataList:res.data.image
+					}
+				)
+			console.log(this.state.dataList)
+		})
+	}
 }
 
-export default MovieStore
+export default connect(
+	null,
+	{
+		changeNavbar(){
+			return {
+				type:"changeNavbar",
+				payload:"Detail"
+			}
+		}
+	}
+
+	)(MovieStore)
