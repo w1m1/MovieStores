699d5be43dd63b7636fba1ec2df4f2b9d2a2e332
wm <1191273496@qq.com>
2018-08-15 18:44:02 +0800

    aa

--- src/Components/MovieDetail/index.js
+++ src/Components/MovieDetail/index.js
@@ -8,7 +8,9 @@ class MovieStore extends Component{
 	constructor(){
 		super();
 		this.state = {
-			dataList:null
+			dataList:null,
+			list:null,
+			isShow:false
 		}
 	}
 	render(){
@@ -17,30 +19,83 @@ class MovieStore extends Component{
 		}
 		return (
 			<div id="MovieStore">
+
 				<div className="bg"  style={mstyle}></div>
 				  <div className="big">
 					<div className="intro">
 						<div className="img">
 								<img src={this.state.dataList}/>
 						</div>
+						{
+							this.state.list?
+							<div className="right">
+								<h2>{this.state.list.titleCn}</h2>
+								<h3>{this.state.list.titleEn}</h3>
+								<p className="min">
+									<span>有彩蛋</span>-<span>{this.state.list.runTime}</span>
+									<span className="store">{this.state.list.rating}</span>
+								</p>
+								<p className="xiju">{this.state.list.type}</p>
+								<p>{this.state.list.release[0],this.state.list.release[1]}</p>
+								<span className="want">我想看</span>
+								<span className="com">我要评分</span>
+							</div>
+							:null
+						}
 						
-						<div className="right">
-							<h2>一出好戏</h2>
-							<h3>The Island</h3>
-							<p className="min">
-								<span>有彩蛋</span>-134分钟
-								<span className="store">7.2</span>
-							</p>
-							<p className="xiju">喜剧</p>
-							<p>2018年8月10日中国上映</p>
-							<span className="want">我想看</span>
-							<span className="com">我要评分</span>
 						</div>
 					</div>
-					</div>
-				
-					<p className="word">黄渤在绝境荒岛的逆袭之路</p>
+				{
+				 this.state.list?
+				  <div>
+					<p className="word">{this.state.list.commonSpecial}</p>
 					<NavLink to="/index" className="btn"><span>查影讯/购票</span></NavLink>
+
+					<div className="content">
+						<p className="brief" style={this.state.isShow?{height:'300px'}:{height:'95px',overflow:'hidden'}}>{this.state.list.content}</p>
+						<p className="changeword" onClick={this.handleClick.bind(this)}>...</p>
+					</div>
+				  </div>
+				  :null
+				}
+
+			{
+				this.state.list?
+				<div className="actor">
+					<h3>12位演职员</h3>
+					<div className="left">
+						<h4>导演</h4>
+						<div>
+							<img src={this.state.list.director.directorImg}/>
+							<span>{this.state.list.director.directorName}</span>
+							<span>{this.state.list.director.directorNameEn}</span>
+						</div>
+
+					</div>
+					<div className="right">
+						<h4>主要演员</h4>
+						<div className="img">
+							<div>
+								<span></span>
+								<span></span>
+							</div>							
+							<div>
+								<span></span>
+								<span></span>
+							</div>							
+							<div>
+								<span></span>
+								<span></span>
+							</div>							
+							<div>
+								<span></span>
+								<span></span>
+							</div>
+						</div>
+					</div>
+				</div>
+				:null
+			}
 			</div>
 
 			)
@@ -55,10 +110,17 @@ class MovieStore extends Component{
 			console.log(res.data)
 			this.setState(
 					{
+						list:res.data,
 						dataList:res.data.image
 					}
 				)
-			console.log(this.state.dataList)
+			console.log(this.state.list)
+		})
+	}
+
+	handleClick(){
+		this.setState({
+			isShow:!this.state.isShow
 		})
 	}
 }
