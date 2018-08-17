5236cccb1da389a318d05026e3fd088e2e47f416
zf <1678209565@qq.com>
2018-08-17 10:32:20 +0800

    Detail完成，添加swiper，更新iconfont

--- src/Components/Detail/index.js
+++ src/Components/Detail/index.js
@@ -11,7 +11,11 @@ class Detail extends Component{
 		this.state = {
 			looplist:[],
 			list:[],
-			isShow:0
+			isShow:0,
+			isS:0,
+			text:null,
+			datelist:[],
+			timelist:[]
 		}
 	}
 	render(){
@@ -20,12 +24,13 @@ class Detail extends Component{
 				{/*影院信息*/}
 				<Header movieName={this.state.list.name}></Header>
 
+
 				{/*影片拖拽移动*/}
 				<div className="swiper-container">
 				    <div className="swiper-wrapper">
 				      	{
 					      	this.state.looplist.map((item,index)=>
-					      		<div className="swiper-slide"  key={item.movieId} onClick={this.handleClick.bind(this,index)}>
+					      		<div className="swiper-slide"  key={item.movieId} onClick={this.handleClick.bind(this,index,item.movieId)}>
 						      		<div className="img">
 						      			<img src={item.img} />
 						      			<em className={item.ratingFinal>0?'':'block'}>{item.ratingFinal}</em>
@@ -37,57 +42,97 @@ class Detail extends Component{
 				      	}
 				    </div>
 				</div>
-
 				{/*影片信息*/}
 				{this.state.looplist.length>0?
 					<div className="title">
 						<div className="left">
 							<h1>{this.state.looplist[this.state.isShow].title}</h1>
-							<p>{this.state.looplist[this.state.isShow].length} - {this.state.looplist[this.state.isShow].type}</p>
+							<p>
+								{this.state.looplist[this.state.isShow].length} - {this.state.looplist[this.state.isShow].type}
+							</p>
 						</div>
-						<i className="iconfont icon-jiantou1"></i>
+						<i className="iconfont icon-jiantou1" onClick={this.toMovieDetail.bind(this)}></i>
 					</div>
 				:null}
+				{/*影片日期*/}
+				<ul className="clear date">
+					{this.state.datelist.map((item,index)=>
+						<li className={this.state.isS===index? 'active':''} key={item} onClick={this.handleClick1.bind(this,index)}>{item}</li>
+					)}
+				</ul>
+
 
 				{/*影片时间*/}
-				<Time></Time>
+				<Time mytime={this.state.timelist}{...this.props}></Time>
+				
+
 				{/*this.props.match.params.movieId*/}
 				{/*this.props.match.params.cinemaId*/}
 			</div>
 
 			)
 	}
-
 	componentWillMount(){
 		this.props.changeNavbar("Detail");
 	}
 	componentDidMount(){
-		// axios.get().then()
 		this.props.changeTitle("")
-		axios.get("/Service/callback.mi/Showtime/ShowtimeMovieAndDateListByCinema.api?cinemaId=3976&t=201881513534121306").then(res=>{
+		axios.get("/Service/callback.mi/Showtime/ShowtimeMovieAndDateListByCinema.api?cinemaId=3976").then(res=>{
 			console.log(res.data)
 			this.setState({
 				looplist:res.data.movies,
-				list:res.data.cinema
+				list:res.data.cinema,
+				datelist:res.data.movies[0].showDates,
+				text:res.data.movies[0].movieId
+
 			},function(){
 				this.Swiper = new Swiper('.swiper-container', {
 				  slidesPerView: 4,
 				  spaceBetween: 30,
 				  centeredSlides: true
 				})
+				// var dateStr = this.state.datelist[0];
+				// var reg = /^(\d{4})-(\d{1,2})-(\d{1,2})$/;
+				// console.log(dateStr.match(reg));
+				// console.log(RegExp.$2);
+				// console.log(RegExp.$3);
+
 			})
 		}).catch(error=>{
 
 		})
-	}
-	handleClick(index){
-		console.log(this.state.looplist[index].movieId)
-
-		this.setState({
-			isShow:index
+		axios.get(`/Service/callback.mi/showtime/ShowTimesByCinemaMovieDate.api?cinemaId=3976&movieId=${this.state.text}&date=2018-08-16&t=20188161513591162`).then(res=>{
+			this.setState({
+				timelist:res.data
+			})
 		})
-		
-
+	}
+	handleClick(index,data){
+		this.setState({
+			isShow:index,
+			text:data,
+			isS:0,
+			datelist:this.state.looplist[index].showDates
+		})
+		axios.get(`/Service/callback.mi/showtime/ShowTimesByCinemaMovieDate.api?cinemaId=3976&movieId=${data}&date=${this.state.datelist[0]}`).then(res=>{
+			this.setState({
+				timelist:res.data
+			})
+		})
+		// console.log(this.state.timelist)
+	}
+	handleClick1(index,item){
+		this.setState({
+			isS:index
+		})
+		axios.get(`/Service/callback.mi/showtime/ShowTimesByCinemaMovieDate.api?cinemaId=3976&movieId=${this.state.text}&date=${this.state.datelist[index]}`).then(res=>{
+			this.setState({
+				timelist:res.data
+			})
+		})
+	}
+	toMovieDetail(){
+		this.props.history.push(`/MovieDetail/${this.state.text}`)
 	}
 }
 export default connect(
